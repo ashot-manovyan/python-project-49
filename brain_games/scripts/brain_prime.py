325346bec41055ca536461ff67d4868af88c9a80
ashot-manovyan <ashot1011@yandex.ru>
2024-06-10 01:05:35 +0400

    fix typo in the message in brain_prime game

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -6,7 +6,7 @@ import brain_games.games.prime
 
 def main():
     brain_games.greetings
-    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     brain_games.engine.quiz(brain_games.games.prime.task)
 
 
